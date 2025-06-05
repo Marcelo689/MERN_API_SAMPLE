bf341c756fe73e16614d852a15016eb9621204a3
Marcelo689 <marcelosj7159@gmail.com>
2025-06-05 17:17:55 -0300

    delete bugado

--- frontend/src/store/product.js
+++ frontend/src/store/product.js
@@ -20,5 +20,19 @@ export const useProductStore = create((set) =>({
 
         set((state) => ({ products: [...state.products, data]}))
         return { success: true, message: "Product created successfully"};
+    },
+    fetchProducts: async () => {
+        const res = await fetch("/api/products");
+        const data = await res.json();
+        set({ products: data.data});
+    },
+    deleteProduct: async (id) => {
+        const res = await fetch("/api/products/"+id,{
+            method:"DELETE"
+        });
+        const data = await res.json();
+        if(!data.success) return { success: false, message: data.message};
+
+        set( state => ({ products: state.products.filter(product => product._id != id)}));
     }
 }))
